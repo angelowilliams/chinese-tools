08af711beb58f3fead6e95e205d09fa713267bc0
Angelo Williams <angelowilliams17@gmail.com>
2023-12-28 03:11:12 -0500

    Add blacklisted lines and remove duplicate lines

--- flashcard-generator/utils/subs2txt.py
+++ flashcard-generator/utils/subs2txt.py
@@ -2,6 +2,10 @@ import pysrt
 
 LATIN_ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
 CHARS_TO_IGNORE = ',.?! 。…'
+# Whisper tends to hallucinate a specific set of tokens. These are the ones
+# I've found so far:
+BLACKLISTED_LINES = ['未经许可,不得翻唱或使用',
+                     '请不吝点赞 订阅 转发 打赏支持明镜与点点栏目']
 
 def subs2txt(input_file, output_file, min_chars=5, no_english=True, verbose=False):
     if verbose:
@@ -21,8 +25,14 @@ def subs2txt(input_file, output_file, min_chars=5, no_english=True, verbose=Fals
             if any([char in LATIN_ALPHABET for char in sub.text]):
                 continue
         
+        if sub.text in BLACKLISTED_LINES:
+            continue
+        
         texts_to_write.append(sub.text)
     
+    # Remove any duplicate lines
+    texts_to_write = list(set(texts_to_write))
+
     output.write('\n'.join(texts_to_write).encode('utf-8'))
     output.close()
 
